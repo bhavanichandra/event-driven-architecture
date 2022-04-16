b0dafbb54491c04d1dba17fa6fee3556ed8ed93c
bhavanichandra <bhavanichandra9@gmail.com>
2022-04-17 02:10:10 +0530

    Added new java project for eventsframework library

---
+++ events-framework/src/main/java/com/themuler/eventsframework/EventsFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.themuler.eventsframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsFrameworkApplication.class, args);
+	}
+
+}
---
+++ events-framework/src/test/java/com/themuler/eventsframework/EventsFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themuler.eventsframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
