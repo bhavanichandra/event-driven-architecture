1cb785348c5192222c244d667dd4d7bd78af6cc1
bhavanichandra <bhavanichandra9@gmail.com>
2022-04-16 20:15:09 +0530

    few changes to controller and added swagger

--- eda-api/src/main.ts
+++ eda-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('To Do App')
+    .setDescription(
+      'Simple ToDo app to test Event Driven Architecture in NestJS',
+    )
+    .setVersion('1.0')
+    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
+    .build();
+  const swagger = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, swagger);
+  await app.listen(8080);
 }
 bootstrap();
